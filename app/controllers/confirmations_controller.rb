f8c3eda322f63120a43dcbf9f8ad273c4ccd7423
Axel Mukwena <axel.muk@gmail.com>
2021-03-07 17:23:03 +0800

    Update confirmations_controller.rb

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,5 +1,5 @@
 class ConfirmationsController < Devise::ConfirmationsController
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
   #protect_from_forgery
 
   private
