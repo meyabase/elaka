637a525d1d73717c8747e2bc21c8ca3988e8c236
Axel Mukwena <axel.muk@gmail.com>
2021-03-07 18:21:18 +0800

    Remove spaces in username forms

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,7 +1,7 @@
 class ConfirmationsController < Devise::ConfirmationsController
   skip_before_action :verify_authenticity_token
   #protect_from_forgery
-  
+
   private
 
   def after_confirmation_path_for(resource_name, resource)
