6b3f569f30c0e69599b4a1693a42f6c3a9b28bc1
Axel Mukwena <axel.muk@gmail.com>
2021-03-07 17:37:36 +0800

    Update confirmations_controller.rb

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,9 +1,9 @@
 class ConfirmationsController < Devise::ConfirmationsController
   skip_before_action :verify_authenticity_token
-  after_action :after_confirmation_path_for
   #protect_from_forgery
-
+  
   private
+
   def after_confirmation_path_for(resource_name, resource)
     sign_in(resource)
     root_path
